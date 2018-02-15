265342759bb7ad3169bc9f8c2d80edeb3f1693aa
Idan S <idansayag@gmail.com>
2018-02-15 11:10:18 +0200

    adding button and Toast message

--- myapplication/MainActivity.java
+++ myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.cp.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 //first application
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//View is an element in th UI - in this case this is the button
+public void doClick(View view){
+    Toast.makeText(this,"This is a Toast",Toast.LENGTH_SHORT).show();
 }
+}
+
+
